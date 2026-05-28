10b5474f83ecb6a165521825820c92611dff9e5f
qFioofa <penavana1@gmail.com>
2026-05-28 16:36:24 +0300

    test niri fix

--- src/hosts/default.nix
+++ src/hosts/default.nix
@@ -15,4 +15,12 @@
 
   networking.hostName = "nixos";
   system.stateVersion = "24.11";
+
+  services.displayManager.sddm.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.autoLogin.enable = true;
+  services.xserver.displayManager.sddm.autoLogin.user = "qFioofa";
+  services.xserver.desktopManager.plasma5.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
 }
--- src/modules/desktop/niri.nix
+++ src/modules/desktop/niri.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   programs.niri.enable = true;
 
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.sessionPackages = [ pkgs.niri ];
+
   environment.sessionVariables = {
     NIXOS_OZONE_WL   = "1";
     XDG_SESSION_TYPE = "wayland";
